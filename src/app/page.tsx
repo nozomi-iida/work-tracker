86f609c9f8a50e17b81d0bc731f848d1f419cdfe
nozomi-iida <iida19990106@gmail.com>
2023-10-19 10:59:12 +1100

    feat: add description in time tracker

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 'use client';
 
 import { Button } from "@/components/ui/button";
-import { useEffect, useState } from "react";
+import { FormEvent, useEffect, useRef, useState } from "react";
 import {v4 as uuidv4} from 'uuid';
 import dayjs from "dayjs";
+import { Dialog, DialogClose, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Textarea } from "@/components/ui/textarea";
 
 const WORK_TIME = "WORK_TIME";
 
@@ -11,11 +13,13 @@ type TimeTracker = {
   id: string;
   started_at: string;
   ended_at?: string;
+  description?: string;
 }
 
 export default function Home() {
   const [data, setData] = useState<TimeTracker[]>([]);
   const [isWorking, setIsWorking] = useState(false);
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
   const onCheckIn = () => {
     const now = new Date().toLocaleString(undefined, {timeZone: "Australia/Sydney"});
     const newData = [...data, {id: uuidv4(), started_at: now}];
@@ -23,14 +27,18 @@ export default function Home() {
     setData(newData);
     setIsWorking(true);
   }
-  const onCheckOut = () => {
+
+  const onCheckOut = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
     const now = new Date().toLocaleString(undefined, {timeZone: "Australia/Sydney"});
     data[data.length - 1].ended_at = now;
+    data[data.length - 1].description = textareaRef.current?.value;
     const newData = [...data];
     localStorage.setItem(WORK_TIME, JSON.stringify(newData));
     setData(newData);
     setIsWorking(false);
   }
+
   const caluculateWorkTime = (item: TimeTracker) => {
     if (!item.ended_at) return;
     const start = dayjs(item.started_at);
@@ -56,12 +64,27 @@ export default function Home() {
     <main>
       <div className="flex space-x-4">
         <Button variant="secondary" onClick={onCheckIn} disabled={isWorking}>Check In</Button>
-        <Button variant="outline" onClick={onCheckOut}>Check Out</Button>
+              <Dialog>
+          <DialogTrigger asChild>
+        <Button variant="outline">Check Out</Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader><DialogTitle>Check Out</DialogTitle></DialogHeader>
+            <form onSubmit={onCheckOut}>
+              <Textarea ref={textareaRef} className="mb-8" />
+              <DialogClose asChild>
+
+        <Button variant="outline">Check Out</Button>
+              </DialogClose>
+            </form>
+          </DialogContent>
+      </Dialog>
       </div>
       {data.map((item) => (
-        <div key={item.started_at}>
+        <div key={item.started_at} className="flex gap-8">
           <p>{dayjs(item.started_at).format("YYYY/MM/DD")}</p>
           <p>WorkTime: {caluculateWorkTime(item)}</p>
+          <p>{item.description}</p>
         </div>
       ))}
     </main>
